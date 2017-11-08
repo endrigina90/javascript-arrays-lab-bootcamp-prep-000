8e097c555cf382314100742308f56be3b0732144
Endri Gina <endrigina90@gmail.com>
2017-11-08 17:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = "I don't do much."
-function destructivelyAppendKitten(array) {
-return appendKitten()
+function destructivelyAppendKitten(array,element) {
+return appendKitten(element)
 }
